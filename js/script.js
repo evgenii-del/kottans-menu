6bf99f54098fdab31f4875c9dcc96e747c035eff
Khasanov <zkhasanovv@gmail.com>
2020-11-04 09:52:03 +0200

    Refactoring

--- js/script.js
+++ js/script.js
@@ -1,54 +1,48 @@
 const cats = [
     {
-        name: 'Sebastian',
+        name: 'Пуфик',
         icon: 'fas fa-cat',
         url: 'https://images.pexels.com/photos/5200916/pexels-photo-5200916.jpeg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-        title: 'Sebastian Tigger'
     },
     {
-        name: 'Smokey',
+        name: 'Люся',
         icon: 'fas fa-paw',
         url: 'https://images.pexels.com/photos/1366996/pexels-photo-1366996.jpeg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-        title: 'Smokey Cleo'
     },
     {
-        name: 'Thomas',
+        name: 'Трейси',
         icon: 'fas fa-fish',
         url: 'https://images.pexels.com/photos/1457595/pexels-photo-1457595.jpeg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-        title: 'Thomas Misty'
     },
     {
-        name: 'Muffin',
+        name: 'Люк',
         icon: 'fas fa-heart',
         url: 'https://images.pexels.com/photos/2870353/pexels-photo-2870353.jpeg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-        title: 'Muffin Sooty'
     },
     {
-        name: 'George',
+        name: 'Спарк',
         icon: 'fas fa-sun',
         url: 'https://images.pexels.com/photos/979247/pexels-photo-979247.jpeg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-        title: 'George Gizmo'
     }
 ]
 
-const main = document.getElementById('main');
-const menu = document.getElementById('menu');
+const main = document.querySelector('.js-main');
+const menu = document.querySelector('.js-menu');
 
 const renderCats = (position) => {
-    let item = cats[position];
+    let cat = cats[position];
     main.innerHTML = `
-        <h1 id="title">${item.title}</h1>
-        <img id="img" src=${item.url} alt=${item.name}>`;
+        <img id="img" src=${cat.url} alt=${cat.name}>`;
 }
 
-const renderItem = (item, index) => {
+const renderItem = (cat, index) => {
     let block = document.createElement('li');
     block.innerHTML = `
         <a href="#" data-id="${index}">
             <span class="icon">
-                <i class="${item.icon}"></i>
+                <i class="${cat.icon}"></i>
             </span>
-            <p class="text">${item.name}</p>
+            <p class="text">${cat.name}</p>
         </a>
     `;
     menu.appendChild(block);
@@ -60,17 +54,19 @@ const removeActive = () => {
     })
 }
 
-cats.forEach((item, index) => {
-    renderItem(item, index);
-})
+window.onload = () => {
+    cats.forEach((item, index) => {
+        renderItem(item, index);
+    })
 
-document.querySelectorAll('li').forEach((li) => {
-    li.addEventListener('click', ({target}) => {
-        removeActive();
-        target.className = 'active';
-        renderCats(target.getAttribute("data-id"));
-    });
-})
+    document.querySelectorAll('li').forEach((li) => {
+        li.addEventListener('click', ({target}) => {
+            removeActive();
+            target.className = 'active';
+            renderCats(target.getAttribute("data-id"));
+        });
+    })
 
-renderCats(0);
-document.querySelector('a').className = 'active';
+    renderCats(0);
+    document.querySelector('a').className = 'active';
+};
